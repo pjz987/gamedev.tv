8de844836a9035e9a3f47f314b863be62129d127
Peter W Jones <pwj2012@gmail.com>
2024-05-02 11:51:42 -0700

    :rocket:

--- ProjectBoost/Player.gd
+++ ProjectBoost/Player.gd
@@ -1,9 +1,21 @@
-extends Node3D
+extends RigidBody3D
+
+## How much vertical force to apply when moving.
+@export_range(750.0, 3000.0) var thrust: float = 1000.0
+@export var torque_thrust: float = 100.0
+
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
-		position.y += delta
-	if Input.is_action_pressed('ui_left'):
-		rotate_z(delta)
-	if Input.is_action_pressed('ui_right'):
-		rotate_z(-delta)
+	if Input.is_action_pressed("boost"):
+		apply_central_force(basis.y * delta * thrust)
+	if Input.is_action_pressed("rotate_left"):
+		apply_torque(Vector3(0.0, 0.0, torque_thrust * delta))
+	if Input.is_action_pressed("rotate_right"):
+		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
+
+
+func _on_body_entered(body: Node) -> void:
+	if "Goal" in body.get_groups():
+		print("body.nameya win bitch!!!!111!!111!!1")
+	elif "Hazard" in body.get_groups():
+		print("the flooooor!!!!")
