a1f93f5ef822f8e55bbd5a4351249d974075b886
Peter W Jones <pwj2012@gmail.com>
2024-05-03 08:17:17 -0700

    save

--- ProjectBoost/Player.gd
+++ ProjectBoost/Player.gd
@@ -4,18 +4,52 @@ extends RigidBody3D
 @export_range(750.0, 3000.0) var thrust: float = 1000.0
 @export var torque_thrust: float = 100.0
 
+var is_transitioning: bool = false
+
+@onready var explosion_audio: AudioStreamPlayer = $ExplosionAudio
+@onready var success_audio: AudioStreamPlayer = $SuccessAudio
+@onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * thrust)
+		if rocket_audio.playing == false:
+			rocket_audio.play()
+	else:
+		rocket_audio.stop()
+
 	if Input.is_action_pressed("rotate_left"):
 		apply_torque(Vector3(0.0, 0.0, torque_thrust * delta))
+
 	if Input.is_action_pressed("rotate_right"):
 		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
 
 
 func _on_body_entered(body: Node) -> void:
-	if "Goal" in body.get_groups():
-		print("body.nameya win bitch!!!!111!!111!!1")
-	elif "Hazard" in body.get_groups():
-		print("the flooooor!!!!")
+	if is_transitioning == false:
+		if "Goal" in body.get_groups():
+			complete_level(body.file_path)
+		elif "Hazard" in body.get_groups():
+			crash_sequence()
+
+
+func crash_sequence() -> void:
+	print("KABOOM!")
+	explosion_audio.play()
+	set_process(false)
+	is_transitioning = true
+	var tween = create_tween()
+	tween.tween_interval(2.5)
+	tween.tween_callback(get_tree().reload_current_scene)
+
+
+func complete_level(next_level_file: String) -> void:
+	print("VICTORY!")
+	success_audio.play()
+	set_process(false)
+	is_transitioning = true
+	var tween = create_tween()
+	tween.tween_interval(1.5)
+	tween.tween_callback(
+		get_tree().change_scene_to_file.bind(next_level_file)
+	)
